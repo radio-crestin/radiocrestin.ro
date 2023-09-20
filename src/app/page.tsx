7af525812f70c45565802d7004bc3f88a417f86e
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-09-20 22:01:37 +0300

    feat: add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
+import BannerApp from "@/components/BannerApp";
+import React from "react";
+import Image from "next/image";
+import github_icon from "public/github-mark-white.svg"
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,21 +11,18 @@ export default function Home() {
       <div className={styles.description}>
 
       </div>
-
       <div className={styles.center}>
         <h1>Revenim în curând.</h1>
-        <h3>Pregătim o noua versiune a website-ului.</h3>
+        <BannerApp />
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://github.com/radio-crestin/radiocrestin.ro"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
+      <div>
+
+        <div className={styles.grid}>
+          <Link href={"https://github.com/radio-crestin/radiocrestin.ro"}>
+            <Image src={github_icon} alt={"Github icon"} width={30} />
+          </Link>
+        </div>
       </div>
     </main>
   )
---
+++ src/components/BannerApp/index.tsx
@@ -0,0 +1,54 @@
+import styles from './styles.module.scss';
+import qr_code from "public/qr-code.webp"
+import playstore from "public/playstore.svg"
+import appstore from "public/appstore.svg"
+import { Simulate } from "react-dom/test-utils";
+import play = Simulate.play;
+
+const BannerApp = () => {
+  return (
+    <div className={styles.container_banner}>
+      <div className={styles.content_left}>
+        <h1 className={styles["chakra-text"]}>
+          Intre timp descarca aplicatia Radio Crestin
+        </h1>
+        <p className={styles["chakra-text"]}>
+          Toate posturile tale preferate într-un singur loc, gratis și fără reclame.
+        </p>
+        <div className={styles.apps_download}>
+          <a href="https://apps.apple.com/app/6451270471">
+            <img
+              alt="AppStore Image Radio Crestin"
+              loading="lazy"
+              width="150"
+              height="47"
+              src={appstore.src}
+            />
+          </a>
+          <a style={{ position: "relative" }}
+            href="https://play.google.com/store/apps/details?id=com.radiocrestin.radio_crestin&amp;hl=en_US">
+            <img
+              alt="PlayStore Image Radio Crestin"
+              loading="lazy"
+              width="150"
+              height="53"
+              src={playstore.src}
+            />
+          </a>
+        </div>
+      </div>
+      <div className={styles.QR_code}>
+        <img
+          alt="QR Code Radio Crestin"
+          loading="lazy"
+          width="90"
+          height="90"
+          src={qr_code.src}
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default BannerApp;
