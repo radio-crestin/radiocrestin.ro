2df79bd1732a94526b4ba6afb16495a348dd6e3c
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-09-23 16:41:51 +0300

    fix: update seo strings

--- src/common/utils/seo.ts
+++ src/common/utils/seo.ts
@@ -2,16 +2,16 @@ import { IStation } from "@/models/Station";
 
 export const seoStation = (station: IStation) => {
   return {
-    title: `${station.title + ' · LIVE  ·'} Radio Crestin `,
-    description: station?.title ? `Asculta ${station.title} live · ⭐ Lista de radiouri crestine · Radio Crestin Live` : SEO_DEFAULT.description,
-    keywords: station?.title ? `${station.title}, ${station.title} live, ${station.title} online, radio crestin, radio-crestin.com, radiouri crestine romanesti, radio crestin online, muzica crestina, lista radio crestin, radio fm crestin` : SEO_DEFAULT.keywords,
+    title: `${station?.title || "Radio Creștin"} | Caută şi ascultă Radiouri Creştine online`,
+    description: station?.title ? `Ascultă ${station.title} online ⭐ RadioCrestin.ro iti ofera o lista de radiouri crestine pentru sufletul tau la un simplu click!` : SEO_DEFAULT.description,
+    keywords: station?.title ? `${station.title}, ${station.title} live, radio crestin, radiocrestin, radiouri crestine, radio crestin online, radiouri crestine online, radio crestine, radiocrestin.ro, online` : SEO_DEFAULT.keywords,
     imageUrl: station?.thumbnail_url || SEO_DEFAULT.imageUrl,
     fullURL: station?.slug ? `https://radiocrestin.ro/${station.slug}` : `https://radiocrestin.ro/`,
   };
 };
 
 export const SEO_DEFAULT = {
-  title: "Radio Crestin | Caută şi ascultă Radiouri Creştine online",
+  title: "Radio Creștin | Caută şi ascultă Radiouri Creştine online",
   description: "RadioCrestin.ro iti ofera o lista de radiouri crestine online. Asculta un radio crestin bun si sanatos pentru sufletul tau la un simplu click!",
   keywords: "radio crestin, radiocrestin, radiouri crestine, radio crestin online, radiouri crestine online, radio crestine, radiocrestin.ro, online",
   imageUrl: "/images/android-chrome-512x512.png"
@@ -22,10 +22,4 @@ export const seoNotFoundPage = {
 
 export const seoInternalErrorPage = {
   title: `A apărut o eroare neașteptată`,
-};
-
-export const seoPrivacyPolicy = {
-  title: `Politica de confidențialitate`,
-  description: `Politica de confidențialitate a site-ului Radio-Crestin.com`,
-  keywords: ``,
 };
