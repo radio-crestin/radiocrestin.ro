6ad87a5c8d59892cdd3972aa22901dba180f1e13
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-10-08 23:26:46 +0300

    fix: typo

--- src/common/components/StationInformation/StationInformation.tsx
+++ src/common/components/StationInformation/StationInformation.tsx
@@ -99,7 +99,7 @@ export default function StationInformation(props: any) {
           value={StationRating}
           activeColor="#fe7f38"
           edit={false}
-          half={true}
+          isHalf={true}
         />
 
         <Text
@@ -179,7 +179,7 @@ export default function StationInformation(props: any) {
           <ModalHeader>Acorda o nota statiei</ModalHeader>
           <Box display={"flex"} px={5}>
             <ReactStars
-              key={`rating-${station.id}`}
+              key={`rating-editable-${station.id}`}
               onChange={(rating: number) => {
                 setUserReviewStars(rating);
               }}
