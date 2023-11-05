97f7e67380991013afd271e320386c8804dc181b
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-11-05 22:56:07 +0200

    fix: add huawei link & target blank

--- src/common/components/DownloadAppBanner/DownloadAppBanner.tsx
+++ src/common/components/DownloadAppBanner/DownloadAppBanner.tsx
@@ -18,6 +18,7 @@ export default function DownloadAppBanner() {
       <Box
         minW={{ md: '390px', lg: '625px' }}
         paddingLeft={{ base: '20px', md: '60px' }}
+        paddingRight={{ base: '20px', md: '0' }}
         paddingBottom={20}>
         <Text fontSize={32} fontWeight={700} letterSpacing={0.7}>
           Descarcă aplicația Radio Creștin
@@ -26,28 +27,46 @@ export default function DownloadAppBanner() {
           Toate posturile tale preferate într-un singur loc, gratis și fără
           reclame.
         </Text>
-        <Box mt={5} display={'flex'} gap={2} alignItems={'center'}>
-          <Link href="https://apps.apple.com/app/6451270471">
-            <img
-              loading={"lazy"}
-              src={'/images/appstore.svg'}
-              width={150}
-              height={54}
-              alt={'AppStore Image Radio Crestin'}
-            />
-          </Link>
-          <Link
-            href="https://play.google.com/store/apps/details?id=com.radiocrestin.radio_crestin&hl=en_US"
-            style={{ position: 'relative' }}>
-            <img
-              loading={"lazy"}
-              className={styles.playstore_link}
-              src={'/images/playstore.svg'}
-              width={150}
-              height={53}
-              alt={'PlayStore Image Radio Crestin'}
-            />
-          </Link>
+        <Box mt={5} display={'flex'} gap={2} alignItems={'center'} w={'100%'} maxWidth={450}>
+          <Box width={'33%'}>
+            <Link href="https://apps.apple.com/app/6451270471" target={"_blank"}>
+              <img
+                loading={"lazy"}
+                src={'/images/appstore.svg'}
+                width={'100%'}
+                height={'auto'}
+                alt={'AppStore Image Radio Crestin'}
+              />
+            </Link>
+          </Box>
+          <Box width={'33%'}>
+            <Link
+              href="https://play.google.com/store/apps/details?id=com.radiocrestin.radio_crestin&hl=en_US"
+              style={{ position: 'relative' }} target={"_blank"}>
+              <img
+                loading={"lazy"}
+                className={styles.playstore_link}
+                src={'/images/playstore.svg'}
+                width={'100%'}
+                height={'auto'}
+                alt={'PlayStore Image Radio Crestin'}
+              />
+            </Link>
+          </Box>
+          <Box width={'33%'}>
+            <Link
+              href="https://appgallery.huawei.com/app/C109055331"
+              style={{ position: 'relative' }} target={"_blank"}>
+              <img
+                loading={"lazy"}
+                className={styles.playstore_link}
+                src={'/images/appgallery.svg'}
+                width={'100%'}
+                height={'auto'}
+                alt={'AppGallery Image Radio Crestin'}
+              />
+            </Link>
+          </Box>
         </Box>
       </Box>
       <Box
