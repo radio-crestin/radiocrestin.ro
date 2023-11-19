79a222a9c9375dcab088f49ddc70025e51360169
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-11-19 08:50:21 +0200

    fix: svg color

--- src/common/components/RadioPlayer/index.tsx
+++ src/common/components/RadioPlayer/index.tsx
@@ -234,11 +234,17 @@ export default function RadioPlayer() {
         return <Loading />;
       case PLAYBACK_STATE.PLAYING:
         return (
-          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9V8h2v8zm4 0h-2V8h2v8z" />
+          <path
+            fill="white"
+            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9V8h2v8zm4 0h-2V8h2v8z"
+          />
         );
       default:
         return (
-          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.5 16.5v-9l7 4.5-7 4.5z" />
+          <path
+            fill="white"
+            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM9.5 16.5v-9l7 4.5-7 4.5z"
+          />
         );
     }
   };
