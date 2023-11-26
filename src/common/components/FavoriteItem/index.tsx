25d6698376b508410fa0697db45ac31efa75a1ea
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-11-26 16:19:08 +0200

    feat: update favourite UI

---
+++ src/common/components/FavoriteItem/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+
+import { IStation } from "@/models/Station";
+import styles from "./styles.module.scss";
+import useStore from "@/store/useStore";
+import { useEffect, useState } from "react";
+import CloseIcon from "@/icons/CloseIcon";
+
+const FavouriteItem = (data: IStation) => {
+  const { favouriteItems, toggleFavourite } = useStore();
+  const [isStationFavourite, setIsStationFavourite] = useState(false);
+
+  useEffect(() => {
+    setIsStationFavourite(favouriteItems.includes(data.slug));
+  }, [data.slug, favouriteItems]);
+
+  return (
+    <Link className={styles.station_item} href={data.slug} scroll={false}>
+      <div className={styles.image_container}>
+        <img
+          src={data.thumbnail_url}
+          alt={`${data.title} | radiocrestin.ro`}
+          loading={"lazy"}
+          height={100}
+          width={100}
+        />
+        <p className={styles.station_name}>{data.title}</p>
+      </div>
+      <div
+        className={styles.favourite_heart_container}
+        onClick={(event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          toggleFavourite(data.slug);
+        }}
+      >
+        <CloseIcon width={16} height={16} scale={1} />
+      </div>
+    </Link>
+  );
+};
+
+export default FavouriteItem;
--- src/common/components/Stations/index.tsx
+++ src/common/components/Stations/index.tsx
@@ -5,6 +5,7 @@ import { IStation } from "@/models/Station";
 import styles from "./styles.module.scss";
 import StationItem from "@/components/StationItem";
 import { Context } from "@/context/ContextProvider";
+import FavoriteItem from "@/components/FavoriteItem";
 
 const Stations = () => {
   const { ctx } = useContext(Context);
@@ -18,19 +19,19 @@ const Stations = () => {
     >
       {ctx.favouriteStations.length > 0 && (
         <div className={styles.favourite_section}>
-          <h1>Favourite:</h1>
+          <h1>Favorite</h1>
           <div className={styles.stations_container}>
             {ctx.favouriteStations.map((station: IStation) => {
               return (
                 <React.Fragment key={`favourite-${station.id}-${station.slug}`}>
-                  <StationItem {...station} />
+                  <FavoriteItem {...station} />
                 </React.Fragment>
               );
             })}
           </div>
+          <hr />
         </div>
       )}
-
       <div className={styles.stations_container}>
         {ctx.stations.map((station: IStation) => {
           return (
