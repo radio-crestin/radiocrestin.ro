0faa4e2c96a68e7edabf8c71418cab1d52931711
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2024-02-25 19:54:05 +0200

    fix: add BugsnagPerformance.

--- src/common/utils/bugsnag.ts
+++ src/common/utils/bugsnag.ts
@@ -2,10 +2,15 @@
 import Bugsnag from "@bugsnag/js";
 import BugsnagPluginReact from "@bugsnag/plugin-react";
 import React from "react";
+import BugsnagPerformance from "@bugsnag/browser-performance";
+
+const BUGSGNAG_API_KEY = "637cd2f71acf28bfc92e9bd1207d0233";
 
 Bugsnag.start({
-  apiKey: "637cd2f71acf28bfc92e9bd1207d0233",
+  apiKey: BUGSGNAG_API_KEY,
   plugins: [new BugsnagPluginReact(React)],
 });
+
+BugsnagPerformance.start({ apiKey: BUGSGNAG_API_KEY });
 
 export { Bugsnag };
