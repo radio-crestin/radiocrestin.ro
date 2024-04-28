d9459e96e757531c2b70b5c1eeb5bc7d8199c29b
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2024-04-28 22:43:47 +0300

    fix: update npm packages & sitemap

--- src/common/utils/bugsnag.ts
+++ src/common/utils/bugsnag.ts
@@ -2,7 +2,6 @@
 import Bugsnag from "@bugsnag/js";
 import BugsnagPluginReact from "@bugsnag/plugin-react";
 import React from "react";
-import BugsnagPerformance from "@bugsnag/browser-performance";
 
 const BUGSGNAG_API_KEY = "03f4178ca7ab19103c56be345643e42c";
 
@@ -10,7 +9,5 @@ Bugsnag.start({
   apiKey: BUGSGNAG_API_KEY,
   plugins: [new BugsnagPluginReact(React)],
 });
-
-BugsnagPerformance.start({ apiKey: BUGSGNAG_API_KEY });
 
 export { Bugsnag };
