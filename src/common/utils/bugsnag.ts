a63ea6512fe6baf02b1477290c303b04c3e87a4b
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2024-04-28 14:07:14 +0300

    fix: update bugsnag api key.

--- src/common/utils/bugsnag.ts
+++ src/common/utils/bugsnag.ts
@@ -4,7 +4,7 @@ import BugsnagPluginReact from "@bugsnag/plugin-react";
 import React from "react";
 import BugsnagPerformance from "@bugsnag/browser-performance";
 
-const BUGSGNAG_API_KEY = "637cd2f71acf28bfc92e9bd1207d0233";
+const BUGSGNAG_API_KEY = "03f4178ca7ab19103c56be345643e42c";
 
 Bugsnag.start({
   apiKey: BUGSGNAG_API_KEY,
