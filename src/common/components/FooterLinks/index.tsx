1113cb8d5441f1ee108795b222649f900f0dea5f
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2024-05-04 00:09:45 +0300

    fix: update ui

--- src/common/components/FooterLinks/index.tsx
+++ src/common/components/FooterLinks/index.tsx
@@ -5,7 +5,20 @@ import styles from "./styles.module.scss";
 export default function FooterLinks() {
   return (
     <div className={styles.container}>
-      <Link href={"/statistici"}>Statistici</Link>
+      <Link href={"https://github.com/radio-crestin"} target="_blank">
+        Github
+      </Link>
+      <Link href={"https://graphql-viewer.radio-crestin.com/"} target="_blank">
+        GraphQL
+      </Link>
+      <Link
+        href={
+          "https://www.figma.com/file/iXXR3dhUjwfDDZH4FlEZgx/radio_crestin_com?type=design&node-id=0-1&mode=design&t=bDAT7O8QYisCX7ln-0"
+        }
+        target="_blank"
+      >
+        Figma
+      </Link>
     </div>
   );
 }
--- src/common/components/HeaderHomepage/index.tsx
+++ src/common/components/HeaderHomepage/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 import styles from "./styles.module.scss";
 import WhatsAppButton from "@/components/WhatsAppButton";
-import InstallMobileAppButton from "@/components/InstallMobileAppButton";
 
 const Navigation = () => (
   <nav className={styles.nav}>
@@ -19,23 +18,8 @@ const Navigation = () => (
         <h1>Radio Creștin</h1>
       </Link>
     </div>
-    {/*<WhatsAppButton />*/}
     <div className={styles.external_links}>
-      <Link href="https://github.com/radio-crestin" target={"_blank"}>
-        <img src="./icons/Github.png" alt="Github icon" />
-      </Link>
-      <Link
-        href="https://graphql-viewer.radio-crestin.com/?endpoint=cc_BYFxAcGcC4HpYOYCcCG5gEcA2A6VATASwHsBaAYyQFNIRCA7Hc4gW1gDcBGRVdbIA&query=cc_I4VwpgTgngBA4mALgZUQQ0QSwPYDsDOMA3jPulnoSZgCYzYQ2QxaIA2YMA7mAEb6ZEnMAFs0mNqUQQwaEQH0QESQAcI2AB5R5ZGXMXKYACzb4d02QqWTERkCN65xbAzezoXbTGTC5IhJnwAYwhMFQpcGECQsIj5NCC41g4osGDQ8JxceS9cAGsYADNZRCUwHIw0xHkVbDIYWrJ8AAovEUEALhgARgAaekZIeV4oLpIVEF4vfCMwGi7omABfAEpiGFoWQRTojIiYXIKJqe9ZuiWYEEyRTmozK4PK3CDtEUIsG7I5FWWYXGwuDU2GgoJhcABzdabD5Vb6kPCQ6h0Jw3Fh2BxOCSuGBoCBYepIv5yTi2eyOZzYi5UmAyABumDAXCoGzoXwghE++DQ4M41K+cXB6iuzM2KM4DCYECkGCy8kQ2HMMrw8kF2GF635ss2EuY1IuQA&variables=cc_N4XyA"
-        target={"_blank"}
-      >
-        <img src="./icons/GraphQLIcon.png" alt="GraphQLIcon icon" />
-      </Link>
-      <Link
-        href="https://www.figma.com/file/iXXR3dhUjwfDDZH4FlEZgx/radio_crestin_com"
-        target={"_blank"}
-      >
-        <img src="./icons/FigmaIcon2.png" alt="Figma icon" />
-      </Link>
+      <WhatsAppButton />
     </div>
   </nav>
 );
@@ -57,20 +41,19 @@ const ContentLeft = () => {
 
 const HeaderHomepage = () => {
   return (
-      <>
-        <InstallMobileAppButton />
-        <header className={styles.container}>
-          <Navigation />
-          <div className={styles.content_section} style={{height: "150px"}}>
-            <ContentLeft />
-          </div>
-          <img
-              className={styles.vector_yellow}
-              src={"/images/vector_yellow.svg"}
-              alt={"vector_yellow"}
-          />
-        </header>
-      </>
+    <>
+      <header className={styles.container}>
+        <Navigation />
+        <div className={styles.content_section} style={{ height: "150px" }}>
+          <ContentLeft />
+        </div>
+        <img
+          className={styles.vector_yellow}
+          src={"/images/vector_lemon.svg"}
+          alt={"vector_yellow"}
+        />
+      </header>
+    </>
   );
 };
 
