cfbd8cd76b005ad4ccd542e5855685dfd1b6fbc3
Nicolae Iosif <7108834+iosifnicolae2@users.noreply.github.com>
2025-01-07 00:48:37 +0200

    feat: add /versetul-zilei path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "export",
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/versetul-zilei',
+        destination: 'https://chat.whatsapp.com/Fpezp7iQlxT2oexlxs3kN4',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
