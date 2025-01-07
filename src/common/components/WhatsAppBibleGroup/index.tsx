989ee2ce43b06021e7bae6738e81215c2b6f8005
Nicolae Iosif <7108834+iosifnicolae2@users.noreply.github.com>
2025-01-07 21:34:13 +0200

    Update index.tsx

--- src/common/components/WhatsAppBibleGroup/index.tsx
+++ src/common/components/WhatsAppBibleGroup/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import {MessageCircle} from 'lucide-react'
 
 export default function WhatsAppBibleGroup() {
     const joinGroup = () => {
