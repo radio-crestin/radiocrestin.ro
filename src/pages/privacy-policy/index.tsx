d6d68483bdedf22436c0ee9b0a89f70094992bdc
Nicolae Iosif <7108834+iosifnicolae2@users.noreply.github.com>
2025-01-12 20:36:51 +0200

    Update index.tsx

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -89,7 +89,7 @@ export default function PrivacyPolicyPage() {
             </ul>
             <h4>Usage Data</h4>
             <p>Usage Data is collected automatically when using the Service.</p>
-            <p>Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address),
+            <p>Usage Data may include information such as Your Device&apos;s Internet Protocol address (e.g. IP address),
                 browser type, browser version, the pages of our Service that You visit, the time and date of Your visit,
                 the time spent on those pages, unique device identifiers and other diagnostic data.</p>
             <p>When You access the Service by or through a mobile device, We may collect certain information
@@ -117,7 +117,7 @@ export default function PrivacyPolicyPage() {
                 </li>
                 <li>
                     <p><strong>To contact You:</strong> To contact You by email, telephone calls, SMS, or other
-                        equivalent forms of electronic communication, such as a mobile application's push notifications
+                        equivalent forms of electronic communication, such as a mobile application&apos;s push notifications
                         regarding updates or informative communications related to the functionalities, products or
                         contracted services, including the security updates, when necessary or reasonable for their
                         implementation.</p>
@@ -178,7 +178,7 @@ export default function PrivacyPolicyPage() {
                 functionality of Our Service, or We are legally obligated to retain this data for longer time
                 periods.</p>
             <h3>Transfer of Your Personal Data</h3>
-            <p>Your information, including Personal Data, is processed at the Company's operating offices and in any
+            <p>Your information, including Personal Data, is processed at the Company&apos;s operating offices and in any
                 other places where the parties involved in the processing are located. It means that this information
                 may be transferred to — and maintained on — computers located outside of Your state, province, country
                 or other governmental jurisdiction where the data protection laws may differ than those from Your
@@ -222,18 +222,18 @@ export default function PrivacyPolicyPage() {
             <p>The security of Your Personal Data is important to Us, but remember that no method of transmission over
                 the Internet, or method of electronic storage is 100% secure. While We strive to use commercially
                 acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.</p>
-            <h2>Children's Privacy</h2>
+            <h2>Children&apos;s Privacy</h2>
             <p>Our Service does not address anyone under the age of 13. We do not knowingly collect personally
                 identifiable information from anyone under the age of 13. If You are a parent or guardian and You are
                 aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We
                 have collected Personal Data from anyone under the age of 13 without verification of parental consent,
                 We take steps to remove that information from Our servers.</p>
             <p>If We need to rely on consent as a legal basis for processing Your information and Your country requires
-                consent from a parent, We may require Your parent's consent before We collect and use that
+                consent from a parent, We may require Your parent&apos;s consent before We collect and use that
                 information.</p>
             <h2>Links to Other Websites</h2>
             <p>Our Service may contain links to other websites that are not operated by Us. If You click on a third
-                party link, You will be directed to that third party's site. We strongly advise You to review the
+                party link, You will be directed to that third party&apos;s site. We strongly advise You to review the
                 Privacy Policy of every site You visit.</p>
             <p>We have no control over and assume no responsibility for the content, privacy policies or practices of
                 any third party sites or services.</p>
